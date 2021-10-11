0b41376efffc9a5e8ff2c2bbecdf388d6586fca4
hassan3111 <ali.hassan@student.moringaschool.com>
2021-10-11 12:49:35 +0300

    add @input decorator

--- Quotes/src/app/quote-detail/quote-detail.component.ts
+++ Quotes/src/app/quote-detail/quote-detail.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Quote } from '@angular/compiler';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-quote-detail',
@@ -6,10 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./quote-detail.component.css']
 })
 export class QuoteDetailComponent implements OnInit {
-
+  @Input() quote!:Quote;
+  @Output() isComplete = new EventEmitter<boolean>();
+  
+  quoteDelete(complete:boolean){
+    this.isComplete.emit(complete)
+  }
   constructor() { }
 
   ngOnInit(): void {
   }
-
 }
